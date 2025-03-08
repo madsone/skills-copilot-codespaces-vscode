28da29feac2afe4a8cb8f86426470b7b74d6e455
madsone <62939084+madsone@users.noreply.github.com>
2025-03-08 16:15:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,49 +1,60 @@
-// create web server
-// 1. require http module
-// 2. create server
-// 3. listen to port
-// 4. create get request
-// 5. create post request
-// 6. create put request
-// 7. create delete request
-// 8. create a response
-// 9. create a response for each request
-// 10. create a response for each request
-// 11. create a response for each request
-// 12. create a response for each request
-// 13. create a response for each request
-// 14. create a response for each request
-// 15. create a response for each request
-// 16. create a response for each request
-// 17. create a response for each request
-// 18. create a response for each request
-// 19. create a response for each request
-// 20. create a response for each request
-// 21. create a response for each request
-// 22. create a response for each request
-// 23. create a response for each request
-// 24. create a response for each request
-// 25. create a response for each request
-// 26. create a response for each request
-// 27. create a response for each request
-// 28. create a response for each request
-// 29. create a response for each request
-// 30. create a response for each request
-// 31. create a response for each request
-// 32. create a response for each request
-// 33. create a response for each request
-// 34. create a response for each request
-// 35. create a response for each request
-// 36. create a response for each request
-// 37. create a response for each request
-// 38. create a response for each request
-// 39. create a response for each request
-// 40. create a response for each request
-// 41. create a response for each request
-// 42. create a response for each request
-// 43. create a response for each request
-// 44. create a response for each request
-// 45. create a response for each request
-// 46. create a response for each request
-// 47. create a response for each request
-// 48. create a
+// Create web server with Express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Data
+const comments = [
+    { id: 1, comment: 'Hello' },
+    { id: 2, comment: 'World' },
+    { id: 3, comment: 'Foo' },
+    { id: 4, comment: 'Bar' },
+    { id: 5, comment: 'Baz' },
+];
+
+// Get all comments
+app.get('/comments', (req, res) => {
+    res.send(comments);
+});
+
+// Get comment by ID
+app.get('/comments/:id', (req, res) => {
+    const comment = comments.find(c => c.id === parseInt(req.params.id));
+    if (!comment) return res.status(404).send('The comment with the given ID was not found.');
+    res.send(comment);
+});
+
+// Create new comment
+app.post('/comments', (req, res) => {
+    const comment = {
+        id: comments.length + 1,
+        comment: req.body.comment
+    };
+    comments.push(comment);
+    res.send(comment);
+});
+
+// Update existing comment
+app.put('/comments/:id', (req, res) => {
+    const comment = comments.find(c => c.id === parseInt(req.params.id));
+    if (!comment) return res.status(404).send('The comment with the given ID was not found.');
+    comment.comment = req.body.comment;
+    res.send(comment);
+});
+
+// Delete comment
+app.delete('/comments/:id', (req, res) => {
+    const comment = comments.find(c => c.id === parseInt(req.params.id));
+    if (!comment) return res.status(404).send('The comment with the given ID was not found.');
+    const index = comments.indexOf(comment);
+    comments.splice(index, 1);
+    res.send(comment);
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
